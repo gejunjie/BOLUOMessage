a748b48e955aff26554ab78c831b77947e3ab11b
gjj <1032479542@qq.com>
2019-05-31 17:39:37 +0800

    none

---
+++ common/src/main/java/com/benboer/boluo/common/widget/adapter/TextWatcherAdapter.java
@@ -0,0 +1,16 @@
+package com.benboer.boluo.common.widget.adapter;
+
+import android.text.Editable;
+import android.text.TextWatcher;
+
+/**
+ * Created by BenBoerBoluojiushiwo on 2019/5/31.
+ */
+public abstract class TextWatcherAdapter implements TextWatcher {
+
+
+    @Override
+    public void afterTextChanged(Editable s) {
+
+    }
+}
---
+++ factory/src/main/java/com/benboer/boluo/factory/data/message/SessionDataSource.java
@@ -0,0 +1,10 @@
+package com.benboer.boluo.factory.data.message;
+
+import com.benboer.boluo.factory.data.DbDataSource;
+import com.benboer.boluo.factory.model.db.Session;
+
+/**
+ * Created by BenBoerBoluojiushiwo on 2019/5/31.
+ */
+public interface SessionDataSource extends DbDataSource<Session> {
+}
