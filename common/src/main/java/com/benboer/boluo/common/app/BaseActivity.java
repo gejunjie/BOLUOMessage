91d1006b74571b9527feca41ae01cbf1885e57d3
gjj <1032479542@qq.com>
2019-06-16 19:58:44 +0800

    关于8.0透明activity的适配

--- common/src/main/java/com/benboer/boluo/common/app/BaseActivity.java
+++ common/src/main/java/com/benboer/boluo/common/app/BaseActivity.java
@@ -1,6 +1,10 @@
 package com.benboer.boluo.common.app;
 
 import android.annotation.SuppressLint;
+import android.app.Activity;
+import android.content.pm.ActivityInfo;
+import android.content.res.TypedArray;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -9,6 +13,8 @@ import androidx.fragment.app.Fragment;
 
 import com.benboer.boluo.common.widget.convention.PlaceHolderView;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.List;
 
 import butterknife.ButterKnife;
@@ -22,6 +28,11 @@ public abstract class BaseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
+        if (Build.VERSION.SDK_INT == Build.VERSION_CODES.O && isTranslucentOrFloating()) {
+            boolean result = fixOrientation();
+
+        }
+
         super.onCreate(savedInstanceState);
         // 在界面未初始化之前调用的初始化窗口
         initWidows();
@@ -38,6 +49,61 @@ public abstract class BaseActivity extends AppCompatActivity {
         }
     }
 
+    private boolean isTranslucentOrFloating() {
+        boolean isTranslucentOrFloating = false;
+        try {
+            int[] styleableRes = (int[]) Class.forName("com.android.internal.R$styleable").getField("Window").get(null);
+            final TypedArray ta = obtainStyledAttributes(styleableRes);
+            Method m = ActivityInfo.class.getMethod("isTranslucentOrFloating", TypedArray.class);
+            m.setAccessible(true);
+            isTranslucentOrFloating = (boolean) m.invoke(null, ta);
+            m.setAccessible(false);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return isTranslucentOrFloating;
+    }
+
+
+    private boolean fixOrientation() {
+        try {
+            Field field = Activity.class.getDeclaredField("mActivityInfo");
+            field.setAccessible(true);
+            ActivityInfo o = (ActivityInfo) field.get(this);
+            o.screenOrientation = -1;
+            field.setAccessible(false);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    @Override
+    public void setRequestedOrientation(int requestedOrientation) {
+        if (Build.VERSION.SDK_INT == Build.VERSION_CODES.O && isTranslucentOrFloating()) {
+//            XLog.i(XLog.BASE, "avoid calling setRequestedOrientation when Oreo.");
+            return;
+        }
+        super.setRequestedOrientation(requestedOrientation);
+
+    }
+
+//
+//    private IntentFilter intentFilter;
+//    private MessageReceiver messageReceiver;
+//    private static final String action = "com.igexin.sdk.action.Whh1CYPnM165RCPKomsQH3";
+//
+//
+//    @Override
+//    protected void onResume() {
+//        super.onResume();
+//        intentFilter = new IntentFilter();
+//        intentFilter.addAction(action);
+//        messageReceiver = new MessageReceiver();
+//        registerReceiver(messageReceiver, intentFilter);
+//    }
+
     /**
      * 初始化窗口
      */
@@ -49,7 +115,6 @@ public abstract class BaseActivity extends AppCompatActivity {
      * 初始化控件调用之前
      */
     protected void initBefore() {
-
     }
 
     /**
